394415f11dbb7fe1fafdc9b227e2af55fa165f50
ththong <ththong@sk-global.biz>
2019-06-10 15:02:53 +0700

    Fix run cookiecutter

--- {{cookiecutter.repo_name}}/buildtools/pygettext.py
+++ {{cookiecutter.repo_name}}/buildtools/pygettext.py
@@ -1,4 +1,3 @@
-{% raw %}
 #! /usr/bin/env python3
 # -*- coding: iso-8859-1 -*-
 # Originally written by Barry Warsaw <barry@python.org>
@@ -6,7 +5,7 @@
 # Minimally patched to make it even more xgettext compatible
 # by Peter Funk <pf@artcom-gmbh.de>
 #
-# 2002-11-22 Jürgen Hermann <jh@web.de>
+# 2002-11-22 Jurgen Hermann <jh@web.de>
 # Added checks that _() only contains string literals, and
 # command line args are resolved to module lists, i.e. you
 # can now pass a filename, a module or package name, or a
@@ -209,7 +208,7 @@ def make_escapes(pass_nonascii):
     global escapes, escape
     if pass_nonascii:
         # Allow non-ascii characters to pass through so that e.g. 'msgid
-        # "Höhe"' would result not result in 'msgid "H\366he"'.  Otherwise we
+        # "Hohe"' would result not result in 'msgid "H\366he"'.  Otherwise we
         # escape any character outside the 32..126 range.
         mod = 128
         escape = escape_ascii
@@ -636,4 +635,3 @@ if __name__ == '__main__':
     # this one creates a warning
     _('*** Seen unexpected token "%(token)s"') % {'token': 'test'}
     _('more' 'than' 'one' 'string')
-{% endraw %}
